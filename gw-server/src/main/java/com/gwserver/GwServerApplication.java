c8a4e7a58a7ad6cd7c679ac4a6095f54467eef43
Onur Altuğ <127160023+OnurAltugg@users.noreply.github.com>
2024-04-09 08:22:01 +0300

    commit gw server

---
+++ gw-server/src/main/java/com/gwserver/GwServerApplication.java
@@ -0,0 +1,13 @@
+package com.gwserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwServerApplication.class, args);
+	}
+
+}
